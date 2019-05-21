fef6e48d605537899919c899485e334a85f0a92f
zyh <zyh1131002466@qq.com>
2019-05-21 22:28:25 +0800

    菜单项使用

---
+++ 7.7/7.7/7.7.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "resource.h"
+
+# define IDM_NEW 10 
+# define IDM_SAVE 11
+# define IDM_SAVEAS 12
+# define IDM_EXIT 13
+# define IDM_SUM 14 
+# define IDM_DEVIA 15
+# define IDM_STDDEVIA 16
+# define IDM_SUMHELP 17 
+# define IDM_DEVIAHELP 18
+# define IDM_STDDEVIAHELP 19
+# define IDM_ABOUT 20
